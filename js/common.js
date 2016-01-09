d5d7f56e3c7ef253ddc31f2a7ed28900c96a5f20
yk <yosizuka1@gmail.com>
2016-01-09 12:10:35 +0900

    インデント改定

--- js/common.js
+++ js/common.js
@@ -4,43 +4,33 @@
 /* --------------------------------------------------
 ページトップ
 -------------------------------------------------- */
-$(function(){
-var widthA = $("nav ul li a").width(); // 円の横幅を取得
-$("nav ul li a").height(widthA); //円の横幅を高さに設定
+$(function() {
+    var widthA = $("nav ul li a").width(); // 円の横幅を取得
+    $("nav ul li a").height(widthA); //円の横幅を高さに設定
 });
 //ウインドウサイズが変わったとき
-$( window ).resize(function(){
-var widthA = $("nav ul li a").width();
-$("nav ul li a").height(widthA);
+$(window).resize(function() {
+    var widthA = $("nav ul li a").width();
+    $("nav ul li a").height(widthA);
 });
 
 
-$(document).ready(function(){
-	var topBtn = $('#page-top');    
-	topBtn.hide();
-	//スクロールが100に達したらボタン表示
-	$(window).scroll(function () {
-			if ($(this).scrollTop() > 100) {
-					topBtn.fadeIn();
-			} else {
-					topBtn.fadeOut();
-			}
-	});
-	//スクロールしてトップ
-	topBtn.click(function () {
-			$('body,html').animate({
-					scrollTop: 0
-			}, 500);
-			return false;
-	});
+$(document).ready(function() {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function() {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
 });
-
-
-
-
-
-
-
-
-
-
